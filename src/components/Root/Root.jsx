a19d15941acf51e216cfcc104512d3acbd7b8274
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-11-02 21:16:34 +0600

    projet done

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,15 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
-import '../../App.css'
+import "../../App.css";
 import Footer from "../Footer/Footer";
 const Root = () => {
+  const navigation = useNavigation();
   return (
     <div>
       <Navbar />
       <div className="mt-24 max-w-7xl mx-4 md:mx-auto">
-        <Outlet />
+        {navigation.state === "loading" ? (
+          <div className="font-bold text-4xl flex items-center justify-center h-screen">Loading...........</div>
+        ) : (
+          <Outlet />
+        )}
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
